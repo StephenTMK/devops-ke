c12134e0681c3b0b8027e06905c7b5cad14ae1ef
StephenTMK <topliquoragency@gmail.com>
2025-11-05 03:03:34 +0300

    kind reinstall for spacelifti interation 0246am

--- k8s_provider.tf
+++ k8s_provider.tf
@@ -14,12 +14,19 @@ variable "k8s_token_b64" {
   type        = string
   default     = ""
   sensitive   = true
-  description = "Base64-encoded bearer token (use when k8s_host is set)"
+  description = "Base64-encoded bearer token (single line)."
+}
+
+locals {
+  k8s_token_clean = (
+    var.k8s_token_b64 != ""
+  ) ? regexreplace(trimspace(base64decode(var.k8s_token_b64)), "\\r|\\n", "") : null
 }
 
 provider "kubernetes" {
-  host        = var.k8s_host    != "" ? var.k8s_host : null
-  token       = var.k8s_token_b64 != "" ? base64decode(var.k8s_token_b64) : null
-  insecure    = var.k8s_host    != "" ? true : null
-  config_path = (var.k8s_host == "" && var.kubeconfig_path != "") ? var.kubeconfig_path : null
+  host             = var.k8s_host != "" ? var.k8s_host : null
+  token            = local.k8s_token_clean
+  insecure         = var.k8s_host != "" ? true : null
+  load_config_file = false
+  config_path      = (var.k8s_host == "" && var.kubeconfig_path != "") ? var.kubeconfig_path : null
 }
