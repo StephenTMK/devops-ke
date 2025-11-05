8e46e1eb8a7ec3790d14a53547f48fa02e779076
StephenTMK <topliquoragency@gmail.com>
2025-11-05 03:06:03 +0300

    kind reinstall for spacelifti interation 0305am

--- k8s_provider.tf
+++ k8s_provider.tf
@@ -24,9 +24,8 @@ locals {
 }
 
 provider "kubernetes" {
-  host             = var.k8s_host != "" ? var.k8s_host : null
-  token            = local.k8s_token_clean
-  insecure         = var.k8s_host != "" ? true : null
-  load_config_file = false
-  config_path      = (var.k8s_host == "" && var.kubeconfig_path != "") ? var.kubeconfig_path : null
+  host        = var.k8s_host != "" ? var.k8s_host : null
+  token       = local.k8s_token_clean
+  insecure    = var.k8s_host != "" ? true : null
+  config_path = (var.k8s_host == "" && var.kubeconfig_path != "") ? var.kubeconfig_path : null
 }
