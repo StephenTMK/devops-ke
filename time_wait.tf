2dd808b14c459ba6b380eccce12149158fc16497
StephenTMK <topliquoragency@gmail.com>
2025-11-08 19:45:09 +0300

    argocd and crossplane interation 1941hrs

--- time_wait.tf
+++ time_wait.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.10"
-    }
-  }
-}
-
 resource "time_sleep" "wait_for_argocd_crds" {
   depends_on      = [helm_release.argocd]
   create_duration = "45s"
--- versions.tf
+++ versions.tf
@@ -9,5 +9,13 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.13"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.10"
+    }
   }
 }
