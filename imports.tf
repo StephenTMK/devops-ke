ad354c4d37672e14ac02731a6dca74d56f0fa374
StephenTMK <topliquoragency@gmail.com>
2025-11-09 16:33:27 +0300

    New IaC stack LocalStack 1633

--- imports.tf
+++ imports.tf
@@ -1,17 +1,1 @@
-# Bring pre-existing namespaces under state so OpenTofu stops trying to create them.
-# (Spacelift will apply these "import blocks" during plan/apply.)
-
-import {
-  to = kubernetes_namespace.demo
-  id = "spacelift-dev"
-}
-
-import {
-  to = kubernetes_namespace.argocd
-  id = "argocd"
-}
-
-import {
-  to = kubernetes_namespace.localstack
-  id = "localstack"
-}
+# intentionally empty for a clean recreate
