883c5285e58ad2d387d719245091a4219ae0aa2a
StephenTMK <topliquoragency@gmail.com>
2025-11-09 22:57:55 +0300

    Status: Mamnagement keys 2257

--- crossplane_secret.tf
+++ crossplane_secret.tf
@@ -1,11 +1,12 @@
 locals {
-  # Only render content when enabled (prevents accidentally leaking empty data)
-  aws_creds_ini = var.enable_crossplane_secret ? <<-EOT
-  [default]
-  aws_access_key_id = ${var.aws_access_key_id}
-  aws_secret_access_key = ${var.aws_secret_access_key}
-  EOT
-  : null
+  # Only render when enabled; otherwise null so nothing is created.
+  aws_creds_ini = var.enable_crossplane_secret ? (
+    <<-EOT
+    [default]
+    aws_access_key_id = ${var.aws_access_key_id}
+    aws_secret_access_key = ${var.aws_secret_access_key}
+    EOT
+  ) : null
 }
 
 resource "kubernetes_secret_v1" "aws_creds_localstack" {
